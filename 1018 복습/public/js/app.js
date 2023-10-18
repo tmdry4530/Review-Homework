b47e894bdf889dd4e8daec5bd3b910842bd14e95
chamdom-blockchain <wjdtmdry88@gmail.com>
2023-10-18 17:31:30 +0900

    d

---
+++ 1018 복습/public/js/app.js
@@ -0,0 +1,21 @@
+import Component from "./core/component.js";
+import Comment from "./src/components/comment.js";
+
+class App extends Component {
+  setup() {}
+  template() {
+    return `
+      <div>header</div>
+      <div>
+        <div data-component='comment'></div>
+      </div>
+      <div>footer</div>
+    `;
+  }
+  mounted() {
+    const commentTarget = document.querySelector('[data-component="comment"]');
+    new Comment(commentTarget);
+  }
+  setEvent() {}
+}
+export default App;
---
+++ 1018 복습/public/js/src/contents/comment/form.js
@@ -0,0 +1,30 @@
+import Component from "../../../core/component.js";
+
+class CommentForm extends Component {
+  setup() {}
+  template() {
+    return `
+      <h4>
+        댓글쓰기
+        <span>(${this.props.length})</span>
+      </h4>
+      <form id="commentFrm" class="commentFrm">
+        <span class="ps_box">
+          <input type="text" name='content' id='content' placeholder="댓글 써" />
+        </span>
+        <button type="submit" class="btn">등록</button>
+      </form>
+    `;
+  }
+  mounted() {}
+  handleSubmit(e) {
+    e.preventDefault();
+    const { value: content } = e.target.content;
+    this.props.insertItem(content);
+  }
+  setEvent() {
+    const handleSubmit = this.handleSubmit.bind(this);
+    this.addEvent("submit", "#commentFrm", handleSubmit);
+  }
+}
+export default CommentForm;
