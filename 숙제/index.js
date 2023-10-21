37c72276a8c151d454697f6fd23154a6e3e8c583
chamdom-blockchain <wjdtmdry88@gmail.com>
2023-10-21 17:23:02 +0900

    perfect

--- 숙제/index.js
+++ 숙제/index.js
@@ -1,9 +1,9 @@
 window.onload = function () {
   loadComments();
-  updateComment();
+  commentCount();
 };
 
-function updateCommentCount() {
+function commentCount() {
   let comments;
   if (localStorage.getItem("comments")) {
     comments = JSON.parse(localStorage.getItem("comments"));
@@ -24,14 +24,14 @@ function addComment() {
   const newComment = {
     id: comments.length,
     text: comment_area.value,
-    date: new Date().toISOString.slice(0, 10),
+    date: new Date().toISOString().slice(0, 10),
   };
 
   comments.push(newComment);
   localStorage.setItem("comments", JSON.stringify(comments));
 
-  updateCommentCount();
-  displayComment(newComment);
+  viewComment(newComment);
+  commentCount();
 }
 
 function loadComments() {
@@ -39,15 +39,137 @@ function loadComments() {
   if (localStorage.getItem("comments")) {
     comments = JSON.parse(localStorage.getItem("comments"));
   } else {
-    comment = [];
+    comments = [];
   }
   for (let i = 0; i < comments.length; i++) {
-    displayComment(comments[i]);
+    viewComment(comments[i]);
   }
 }
 
-function displayComment(comment) {
+function viewComment(comment) {
   const container = document.getElementById("container");
 
-  const user_comment_layer = document.getElementById("user_comment");
+  const userCommentLayer = document.createElement("div");
+  userCommentLayer.classList.add("comment-" + comment.id, "user_comment");
+  userCommentLayer.style.display = "block";
+
+  const userId = document.createElement("div");
+  userId.classList.add("user_id");
+  userId.innerText = "User" + comment.id;
+  userCommentLayer.appendChild(userId);
+
+  const commentArea = document.createElement("div");
+  commentArea.classList.add("comment_area");
+
+  const commentElement = document.createElement("span");
+  commentElement.classList.add("comment_text");
+  commentElement.id = "comment-text-" + comment.id;
+  commentElement.textContent = comment.text;
+  commentArea.appendChild(commentElement);
+
+  const modifyInput = document.createElement("input");
+  modifyInput.type = "text";
+  modifyInput.classList.add("modify_input");
+  modifyInput.style.display = "none";
+  commentArea.appendChild(modifyInput);
+
+  const saveButton = document.createElement("button");
+  saveButton.classList.add("save_button");
+  saveButton.innerText = "✔️";
+  saveButton.style.display = "none";
+  commentArea.appendChild(saveButton);
+
+  const modifyButton = document.createElement("button");
+  modifyButton.classList.add("modify");
+  modifyButton.innerText = "✏️";
+  commentArea.appendChild(modifyButton);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("delete");
+  deleteButton.innerText = "🗑️";
+  commentArea.appendChild(deleteButton);
+
+  userCommentLayer.appendChild(commentArea);
+
+  const date = document.createElement("div");
+  date.classList.add("date");
+  date.innerText = comment.date;
+  userCommentLayer.appendChild(date);
+
+  container.appendChild(userCommentLayer);
+
+  saveButton.addEventListener("click", function () {
+    updateComment(comment.id, modifyInput.value);
+    const textCopy = document.getElementById("comment-text-" + comment.id);
+    if (textCopy) {
+      textCopy.textContent = modifyInput.value;
+      textCopy.style.display = "block";
+      modifyInput.style.display = "none";
+      saveButton.style.display = "none";
+    }
+  });
+
+  modifyButton.addEventListener("click", function () {
+    const textCopy = document.getElementById("comment-text-" + comment.id);
+    if (textCopy) {
+      textCopy.style.display = "none";
+      modifyInput.style.display = "block";
+      modifyInput.value = textCopy.textContent;
+      saveButton.style.display = "block";
+    }
+  });
+
+  deleteButton.addEventListener("click", function () {
+    remove(comment.id);
+    const commentElement = document.querySelector(".comment-" + comment.id);
+    if (commentElement) {
+      commentElement.remove();
+    }
+    updateCommentCount();
+  });
+}
+
+function showInput(id) {
+  const textCopy = document.getElementById("comment-text-" + id);
+
+  if (textCopy) {
+    textCopy.style.display = "none";
+    const editArea = textCopy.parentNode;
+    const modifyInput = editArea.querySelector(".edit_input");
+    if (modifyInput) {
+      modifyInput.style.display = "block";
+      modifyInput.value = textCopy.textContent;
+    }
+    let saveButton = editArea.querySelector(".save_button");
+    if (saveButton) {
+      saveButton.style.display = "block";
+    }
+  }
+}
+
+function updateComment(id, newText) {
+  const comments = JSON.parse(localStorage.getItem("comments"));
+  for (let i = 0; i < comments.length; i++) {
+    if (comments[i].id == id) {
+      comments[i].text = newText;
+      localStorage.setItem("comments", JSON.stringify(comments));
+      break;
+    }
+  }
+}
+
+function remove(id) {
+  const comments = JSON.parse(localStorage.getItem("comments"));
+  for (let i = 0; i < comments.length; i++) {
+    if (comments[i].id == id) {
+      comments.splice(i, 1);
+      break;
+    }
+  }
+  localStorage.setItem("comments", JSON.stringify(comments));
+  const commentElement = document.querySelector(".comment-" + id);
+  if (commentElement) {
+    commentElement.remove();
+  }
+  commentCount();
 }
